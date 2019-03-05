024ea5ed8515e105b09df205ce1924154cf9379b
Breshna-Rahimzy-Sheridan <rahimzy@sheridancollege.ca>
2019-03-04 23:09:10 -0500

    initializing number and some notes.added encapsulation.

---
+++ src/practice4/car1.java
@@ -0,0 +1,39 @@
+/*
+ * Breshna Rahimzy
+ * Student ID: 991487145
+ *     SYST10199 - WEB Programming
+ */
+package practice4;
+
+public class car1 {
+    private double gas;
+    private int pmp;
+
+    /**
+     * @return the gas
+     */
+    public double getGas() {
+        return gas;
+    }
+
+    /**
+     * @param gas the gas to set
+     */
+    public void setGas(double gas) {
+        this.gas = gas;
+    }
+
+    /**
+     * @return the pmp
+     */
+    public int getPmp() {
+        return pmp;
+    }
+
+    /**
+     * @param pmp the pmp to set
+     */
+    public void setPmp(int pmp) {
+        this.pmp = pmp;
+    }
+}
